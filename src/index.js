2c00ef4f1e3d9e3cf9b12bfbfdddfea8f4c76929
Gyan Prakash <gyanprakash.gp98010@gmail.com>
2020-09-21 18:52:29 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 function generateCodeFromObject(obj){
     //return a code generated string
+    return React.createElement("div", {
+        name: "Clock",
+        style: {
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center"
+        }
+      }, /*#__PURE__*/React.createElement("div", {
+        name: "Hour"
+      }), /*#__PURE__*/React.createElement("span", {
+        name: "Minute",
+        style: {
+          color: "green",
+          fontSize: "30"
+        }
+      }));
    }
    
    module.exports=generateCodeFromObject;
